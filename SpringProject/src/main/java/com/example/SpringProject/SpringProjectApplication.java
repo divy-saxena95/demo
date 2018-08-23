727d69767925f847f7acce77318522142763175a
Divy Saxena <divysaxena2@gmail.com>
2018-08-23 16:27:54 +0530

    Initial Commit

---
+++ SpringProject/src/main/java/com/example/SpringProject/SpringProjectApplication.java
@@ -0,0 +1,21 @@
+package com.example.SpringProject;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringProjectApplication {
+
+	public static void main(String[] args) {
+
+		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Cat cat = (Cat) context.getBean("cat");
+		System.out.println(cat.toString());
+		Dog dog = (Dog) context.getBean("dog");
+		System.out.println(dog.toString());
+		Frog frog = (Frog) context.getBean("frog");
+		System.out.println(frog.toString());
+		Tiger tiger = (Tiger) context.getBean("tiger");
+		System.out.println(tiger.toString());
+		context.close();
+	}
+}
